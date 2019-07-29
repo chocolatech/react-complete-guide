e5d3c9c6eb41aaef1bcd5c48080ca0aee081df0a
Ewelina Gońda-Warzycha <egonda@sii.pl>
2019-07-29 15:23:40 +0200

    add conditional rendering

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,10 +2,19 @@ import React, { Component } from 'react';
 
 import './Blog.css';
 import Posts from './Posts/Posts'
-import NewPost from './NewPost/NewPost'
-import { Route, NavLink, Switch } from 'react-router-dom'
+import asyncComponent from '../../hoc/asyncComponent'
+// import NewPost from './NewPost/NewPost'
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom'
+
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost');
+})
 
 class Blog extends Component {
+
+    state = {
+        auth: true
+    }
 
     render() {
 
@@ -30,8 +39,11 @@ class Blog extends Component {
                 </header>
 
                 <Switch>
-                    <Route path="/new-post" component={NewPost} />
+                    {this.state.auth ? <Route path="/new-post" component={AsyncNewPost} /> : null}
                     <Route path="/posts" component={Posts} />
+                    <Route render={() => <h1>Not found!</h1>} />
+                    {/* <Redirect from="/" to="/posts" /> */}
+                    {/* <Route path="/" component={Posts} /> */}
                 </Switch>
             </div>
         );
